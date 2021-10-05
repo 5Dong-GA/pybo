9a33d91979d4d379218b897a2a8f3f3471945240
5Dong-GA <gljhan123@gmail.com>
2021-10-05 15:29:41 +0900

    Allowed Host Change

--- web_test/settings.py
+++ web_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-15#ucycct_)v!m%x0ef_ux-q)#66*vx1-k=nq4xc#)r=q&lbx+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.209.217.249']
 
 
 # Application definition
